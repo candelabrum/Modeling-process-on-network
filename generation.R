052406b38188992c3746d6646f2b5d5ad8188523
Pedashenko Vladislav <pedashenkove@yandex.ru>
2021-03-18 00:46:55 +0300

    add states

--- generation.R
+++ generation.R
@@ -1,5 +1,70 @@
 library(sand)
 
+get_cost_seq <- function(k)
+{
+    if (k < 200 && k > 500)
+        return (1);
+    return (10);
+} 
+
+change_state <- function(A_k_m, degree, cost_seq_vert, state)
+{
+    if (state == 1)
+        return (cost_seq_vert <= A_k_m)
+
+    return rbinom(1, 1, 0.3);
+}
+
+get_A_k_m <- function(adj_vector, old_infl)
+{
+    sum = 0;
+    for (v in adj_vector)
+    {
+        sum  = sum + old_infl[v]
+    }
+
+    return (sum);
+}
+
+update_states <- function(g, k)
+{
+    cost_seq <- runif(100, 0, get_cost(k));
+    for (i in 1:100)
+    {
+        adj_v <- adjacent_vertices(g, i)
+        adj_vector <- adj_v[[1]]
+        A_k_m <- get_A_k_m(adj_vector,  V(g)$infected)
+        state <- change_state(A_k_m, degree(g)[i], cost_seq[i], 
+                                        V(g)$infected[i]);
+        V(g)$infected[i] <- state;
+    }
+
+    return (g)
+}
+
+main <- function()
+{
+    set.seed(42)
+    g.er <- erdos.renyi.game(100, 0.10)
+    
+    for (k in 1:1000)
+        g <- update_states(g, k)
+
+    X11();
+    influenced <- rbinom(100, 1, 0.20);
+    g.er$influenced <- influenced
+    g.er.colors <- as.character(length(influenced));
+    g.er.colors[influenced == 1] <- "green";
+    g.er.colors[influenced == 0] <- "deepskyblue";
+    influenced <- update_influenced(g.er, influenced)
+    plot(g.er, layout=layout.fruchterman.reingold,vertex.color=g.er.colors)
+    print(influenced)
+
+    Sys.sleep(100);
+
+#write update influenced and recalculate smth properties of graph
+}
+
 update_influenced <- function(g, old_infl)
 {
     inf_distr <- rep(0, 100)
@@ -12,37 +77,25 @@ update_influenced <- function(g, old_infl)
         sum = 0;
         adj_v <- adjacent_vertices(g, i)
         adj_vector <- adj_v[[1]]
-        #for (v in adj_vector)
-        #{
-        #    sum  = sum + old_infl[v]
-        #}
-        sum = old_infl[i]
-        tmp <- inf_distr[100*degree_distribution(g)[i] + 1] + sum
-        inf_distr[100*degree_distribution(g)[i] + 1] <- tmp
+        A_k_m <- get_A_k_m(adj_vector, old_infl)
+        #
+        #tmp <- inf_distr[100*degree_distribution(g)[i] + 1] + sum
+        #inf_distr[100*degree_distribution(g)[i] + 1] <- tmp
+         
     }
     max_degree <- length(degree_distribution(g))
     print(inf_distr)
+    res = c(1:100);
     for(i in 1:max_degree)
     {
         print("Degree")
         print(degree_distribution(g)[i])
+        res[i] = 0;
         if (degree_distribution(g)[i] != 0)
-            print(inf_distr[i]/(100*degree_distribution(g)[i]))
+            res[i] = inf_distr[i]/(100*degree_distribution(g)[i])
     }
+
+    return (res)
 }
 
-set.seed(42)
-g.er <- erdos.renyi.game(100, 0.10)
 
-X11();
-influenced <- rbinom(100, 1, 0.20);
-g.er$influenced <- influenced
-g.er.colors <- as.character(length(influenced));
-g.er.colors[influenced == 1] <- "green";
-g.er.colors[influenced == 0] <- "deepskyblue";
-update_influenced(g.er, influenced)
-plot(g.er, layout=layout.fruchterman.reingold, vertex.color=g.er.colors)
-print(influenced)
-
-Sys.sleep(100);
-#write update influenced and recalculate smth properties of graph
