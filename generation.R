f99746c11ca69bbd61091c227dc2615fa7294807
Pedashenko Vladislav <pedashenkove@yandex.ru>
2021-03-20 00:07:08 +0300

    almost done

--- generation.R
+++ generation.R
@@ -1,11 +1,102 @@
 library(sand)
 
+fact <- function(n)
+{
+    if (n <= 1)
+        return (1);
+
+    return (n*fact(n-1));
+}
+
+binom <- function(n, m)
+{
+    return (fact(n)/(fact(n-m)*fact(m)));
+}
+
+
 get_cost_seq <- function(k)
 {
     if (k < 20 || k > 50)
         return (1);
     return (10);
 } 
+
+p_01 <- function(a, k)
+{
+    return (min(a/get_cost_seq(k), 1));
+}
+
+emp_theta <- function(emp_ro_k, g)
+{
+    res = 0;
+    denominator = 0;
+
+    P_d <- get_P_d(g);
+    ro_k <- emp_ro_k;
+    D <- length(degree(g));
+
+    for(d in 1:min(length(P_d), length(ro_k), D))
+    {
+        res = res +  d*P_d[d]*ro_k[d];
+        denominator = denominator + d*P_d[d];
+    }
+    #print("P_d\n")
+    #print(P_d)
+    #print("Denominator\n")
+    #print(denominator)
+    #print(res)
+
+    return (res/denominator)
+}
+
+emp_p_01 <- function(emp_ro_k, d, g, k)
+{
+    sum = 0;
+    emp_theta <- emp_theta(emp_ro_k, g)
+
+    for (a in 0:d)
+    {
+        sum = sum + p_01(a, k)*binom(d, a)*(emp_theta ** a)*
+                            ((1 - emp_theta) ** (d-a));
+    }
+    
+    return ((1-emp_ro_k[d])*sum);
+}
+
+p_10 <- function()
+{
+    return (1/3);
+}
+
+emp_p_10 <- function(emp_ro_k, d, g, k)
+{
+    sum = 0;
+    emp_theta <- emp_theta(emp_ro_k, g)
+
+    for (a in 0:d)
+    {
+        sum = sum + p_10()*binom(d, a)*(emp_theta ** a)*
+                            ((1 - emp_theta) ** (d-a));
+    }
+    
+    return ((emp_ro_k[d])*sum);
+}
+
+update_emp_ro_k <- function(emp_ro_k, g, k)
+{
+    D <- length(degree_distribution(g));
+    print(D)
+    ro_k <- emp_ro_k
+    if (ro_k[90] > 0)
+        exit();
+    for(d in 1:D)
+    {
+        ro_k[d] <- ro_k[d] + (1/100)*(emp_p_01(emp_ro_k, d, g, k) - 
+                                        emp_p_10(emp_ro_k, d, g, k));
+    }
+
+    return (ro_k);
+}
 
 change_state <- function(A_k_m, degree, cost_seq_vert, state)
 {
@@ -88,17 +179,20 @@ update_states <- function(g, k)
     return (g)
 }
 
+#get_theory_p_01_ <- function(
 main <- function()
 {
     set.seed(42)
     proba <- vector()
-    g.er <- erdos.renyi.game(100, 0.10)
+    g.er <- erdos.renyi.game(100, 0.20)
 
     influenced <- rbinom(100, 1, 0.05);
     g.er$influenced <- influenced
-
+    
+    ro_k <- get_ro_k(g.er);
     theta <- vector() 
     X11()
+    emp_theta <- vector();
     for(k in 1:100)
     {
         print(k);
@@ -108,20 +202,30 @@ main <- function()
         g.er.colors[g.er$influenced == 1] <- "green";
         g.er.colors[g.er$influenced == 0] <- "deepskyblue";
         influenced <- update_influenced(g.er, influenced)
-        print("influenced")
         proba[k] <- sum(g.er$influenced)/100;
         #plot(g.er, layout=layout.fruchterman.reingold,
         #            vertex.color=g.er.colors)
         #print(g.er$influenced)
+        if (k <= 5)
+        {
+            emp_ro_k <- update_emp_ro_k(get_ro_k(g.er), g.er, k);
+        }
+        if (k > 5) 
+            emp_ro_k <- update_emp_ro_k(emp_ro_k, g.er, k);
+        
+        emp_theta[k] <- emp_theta(emp_ro_k, g.er);
+        #plot(emp_ro_k)
             
-
+         
         theta[k] <- get_infected_link_prob(g.er);
-        #Sys.sleep(5);
     }
+
     print(theta)
     par(mfrow = (c(2, 1)))
-    plot(theta)
-    plot(proba)
+    plot(theta, main = "Real Infected probabily link",
+         col = "deepskyblue", type = "l")
+    plot(emp_theta, main = "Theorical estimate",
+                col = "deepskyblue", type = "l")
 #write update influenced and recalculate smth properties of graph
 }
 
